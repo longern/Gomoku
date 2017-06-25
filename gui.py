b16cd926230af4ba43cad01a3384f17cc60f818d
zerotrac <734688610@qq.com>
2017-06-25 19:56:52 +0800

    添加落棋位置和上一手的落点标记

--- gui.py
+++ gui.py
@@ -1,7 +1,7 @@
 import os
 os.environ["QT_AUTO_SCREEN_SCALE_FACTOR"] = "1"
 from PyQt5 import QtCore, QtGui, QtWidgets
-from ui_board import Ui_Board
+from ui.ui_board import Ui_Board
 from board import Board
 
 class Ui(QtWidgets.QWidget):
@@ -15,6 +15,9 @@ class Ui(QtWidgets.QWidget):
 		self.ui = Ui_Board()
 		self.ui.setupUi(self)
 		self.board = Board()
+		self.setMouseTracking(True)
+		self.cursor_x = -1
+		self.cursor_y = -1
 
 	def paintEvent(self, QPaintEvent):
 		painter = QtGui.QPainter(self)
@@ -27,7 +30,38 @@ class Ui(QtWidgets.QWidget):
 			self.drawStar(painter, i)
 		for i, _ in enumerate(self.board.history):
 			self.drawChessPiece(painter, self.board.history[i], QtCore.Qt.white if i % 2 else QtCore.Qt.black)
+		self.drawCursorPosition(painter, (self.cursor_x, self.cursor_y))
+		if len(self.board.history) > 0:
+			self.drawLastPlayPosition(painter, self.board.history[len(self.board.history) - 1])
 		return super().paintEvent(QPaintEvent)
+
+	def drawLastPlayPosition(self, painter, center):
+		print(center)
+		painter.setPen(QtCore.Qt.red)
+		painter.translate(center[0] * self.gridSize[0] + self.boardPos[0], center[1] * self.gridSize[1] + self.boardPos[1])
+		seg = self.gridSize[0] / 8
+		thres = 1
+		painter.drawLine(0, -seg, 0, -thres)
+		painter.drawLine(0, seg, 0, thres)
+		painter.drawLine(-seg, 0, -thres, 0)
+		painter.drawLine(seg, 0, thres, 0)
+		painter.resetTransform()
+
+	def drawCursorPosition(self, painter, center):
+		if not self.board.in_board(center):
+			return
+		painter.setPen(QtCore.Qt.black)
+		painter.translate(center[0] * self.gridSize[0] + self.boardPos[0], center[1] * self.gridSize[1] + self.boardPos[1])
+		seg = self.gridSize[0] / 4
+		painter.drawLine(-seg * 2, -seg * 2, -seg, -seg * 2)
+		painter.drawLine(-seg * 2, -seg * 2, -seg * 2, -seg)
+		painter.drawLine(seg * 2, -seg * 2, seg, -seg * 2)
+		painter.drawLine(seg * 2, -seg * 2, seg * 2, -seg)
+		painter.drawLine(-seg * 2, seg * 2, -seg, seg * 2)
+		painter.drawLine(-seg * 2, seg * 2, -seg * 2, seg)
+		painter.drawLine(seg * 2, seg * 2, seg, seg * 2)
+		painter.drawLine(seg * 2, seg * 2, seg * 2, seg)
+		painter.resetTransform()
 
 	def drawChessPiece(self, painter, center, color):
 		painter.setBrush(color)
@@ -42,6 +76,14 @@ class Ui(QtWidgets.QWidget):
 		rect = QtCore.QRectF(-self.starSize[0] / 2, -self.starSize[0] / 2, self.starSize[0], self.starSize[0]);
 		painter.drawChord(rect, 0, 5760);
 		painter.resetTransform()
+
+	def mouseMoveEvent(self, QMouseEvent):
+		if self.board.winner:
+			return super().mouseMoveEvent(QMouseEvent)
+		self.cursor_x = int(round((QMouseEvent.x() - self.boardPos[0]) / self.gridSize[0]))
+		self.cursor_y = int(round((QMouseEvent.y() - self.boardPos[1]) / self.gridSize[1]))
+		self.update()
+		return super().mouseMoveEvent(QMouseEvent)
 
 	def mousePressEvent(self, QMouseEvent):
 		if self.board.winner:
