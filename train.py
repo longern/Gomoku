368b44c17bb9386434aff8b8977ea8483217c76c
Siyu Long <771815005@qq.com>
2017-07-02 11:04:20 +0800

    New policy network structure

--- train.py
+++ train.py
@@ -17,39 +17,47 @@ def get_training_data():
 		board = Board()
 		for move in history:
 			features.append(get_features(board))
-			turns.append(*to_categorical(board.current_player - 1, 2))
 			label = numpy.zeros(board.data.shape, int)
 			label[move] = 1
 			labels.append(label)
 			board.play(move)
-	return [numpy.array(features), numpy.array(turns)], numpy.array(labels)
+	return numpy.array(features), numpy.array(labels)
 
-x_train, y_train = get_training_data()
-numpy.save("board.npy", x_train[0])
-numpy.save("turn.npy", x_train[1])
-numpy.save("choice.npy", y_train)
-"""
-x_train = [numpy.load("board.npy"), numpy.load("turn.npy")]
-y_train = numpy.load("choice.npy")
-"""
+generate_sgf_data = False
+if generate_sgf_data:
+	x_train, y_train = get_training_data()
+	numpy.save("board.npy", x_train)
+	numpy.save("choice.npy", y_train)
+else:
+	x_train = numpy.load("board.npy")
+	y_train = numpy.load("choice.npy")
 
-x_train[0] = numpy.concatenate((x_train[0], numpy.flip(x_train[0], 1), numpy.flip(x_train[0], 2)))
-x_train[1] = numpy.concatenate((x_train[1], x_train[1], x_train[1]))
+x_train = numpy.concatenate((x_train, numpy.flip(x_train, 1), numpy.flip(x_train, 2)))
 y_train = y_train.reshape((-1, 15, 15))
 y_train = numpy.concatenate((y_train, numpy.flip(y_train, 1), numpy.flip(y_train, 2))).reshape((-1, 225))
 
 print("Data Loaded")
-input1 = keras.layers.Input(shape=(15, 15, channel_size))
-input2 = keras.layers.Input(shape=(2,))
-layer = keras.layers.Conv2D(4, (3, 3), activation="relu")(input1)
-layer = keras.layers.Dropout(0.25)(layer)
-layer = keras.layers.Flatten()(layer)
-layer = keras.layers.concatenate([layer, input2])
-layer = keras.layers.Dense(225, activation="softmax")(layer)
-model = keras.models.Model(inputs=[input1, input2], outputs=layer)
 
-model.compile("sgd", keras.losses.categorical_crossentropy, metrics=["top_k_categorical_accuracy"])
+load_model = False
+if not load_model:
+	input1 = keras.layers.Input(shape=(15, 15, channel_size))
+	layer = keras.layers.ZeroPadding2D(2)(input1)
+	layer = keras.layers.Conv2D(48, (5, 5), activation="relu")(layer)
+	layer = keras.layers.ZeroPadding2D(1)(layer)
+	layer = keras.layers.Conv2D(48, (3, 3), activation="relu")(layer)
+	layer = keras.layers.ZeroPadding2D(1)(layer)
+	layer = keras.layers.Conv2D(48, (3, 3), activation="relu")(layer)
+	layer = keras.layers.ZeroPadding2D(1)(layer)
+	layer = keras.layers.Conv2D(48, (3, 3), activation="relu")(layer)
+	layer = keras.layers.ZeroPadding2D(1)(layer)
+	layer = keras.layers.Conv2D(48, (3, 3), activation="relu")(layer)
+	layer = keras.layers.LocallyConnected2D(1, (1, 1))(layer)
+	layer = keras.layers.Reshape((225, ))(layer)
+	layer = keras.layers.Activation("softmax")(layer)
+	model = keras.models.Model(inputs=input1, outputs=layer)
+	model.compile("sgd", keras.losses.categorical_crossentropy, metrics=["acc", "top_k_categorical_accuracy"])
+else:
+	model = keras.models.load_model("policy.h5")
 
-#model = keras.models.load_model("model.h5")
-model.fit(x_train, y_train, epochs=50, validation_split=0.1)
-model.save("model.h5")
+model.fit(x_train, y_train, epochs=30, validation_split=0.1, callbacks=[keras.callbacks.EarlyStopping(patience=2)])
+model.save("policy.h5")
