1bbd707ba1fb77c453eaf00e366c57a9b9fecb18
Siyu Long <771815005@qq.com>
2017-07-05 16:00:01 +0800

    Add feature cache

--- gomoku_ai.py
+++ gomoku_ai.py
@@ -1,21 +1,30 @@
-import keras
 import numpy
 from board import Board
 from feature import get_features
-from keras.utils import to_categorical
-model = keras.models.load_model("model.h5")
+from model_manager import model_manager
 
 gauss_noise = lambda scale, shape: numpy.random.normal(0, scale, shape)
 
-def ai_move(board: Board):
+def ai_move(board: Board, feature = None, policy:str = "max"):
 	if board.turn == 1:
 		return naive_ai(board)
+	if feature is None:
+		feature = get_features(board)
 	score = lambda x:naive_score(x, board.current_player)
-	result = model.predict([numpy.array([get_features(board)]), numpy.array(to_categorical(board.current_player - 1, 2))]).reshape(15, 15) + \
-		numpy.apply_along_axis(score, 2, get_features(board)) + \
-		gauss_noise(0.1 * numpy.exp(-board.turn), board.shape)
+	result = model_manager["reinforce"].predict(numpy.array([feature])).reshape(15, 15)
+	if policy != "sample":
+		result += numpy.apply_along_axis(score, 2, feature)
+	result = result * (board.data == 0)
+	result /= result.sum()
+	if policy == "max":
+		choice = numpy.unravel_index(result.argmax(), board.shape)
+	elif policy == "sample":
+		choice = numpy.unravel_index(numpy.random.choice(225, p=result.flatten()), board.shape)
+	return choice
+
+def random_move(board: Board):
 	mask = (board.data == 0)
-	return numpy.unravel_index((result * mask).argmax(), board.shape)
+	return numpy.unravel_index(numpy.random.choice(225, p=mask.flatten()/mask.sum()), board.shape)
 
 def naive_score(x: numpy.array, player: int):
 	enemy = 3 - player
@@ -32,8 +41,10 @@ def naive_score(x: numpy.array, player: int):
 	else:
 		return numpy.argmax(x[3:9]) * 10e-4
 
-def naive_ai(board: Board):
+def naive_ai(board: Board, feature = None):
+	if feature is None:
+		feature = get_features(board)
 	score = lambda x:naive_score(x, board.current_player)
-	result = numpy.apply_along_axis(score, 2, get_features(board)) + gauss_noise(10e-6, board.shape)
+	result = numpy.apply_along_axis(score, 2, feature) + gauss_noise(10e-6, board.shape)
 	mask = (board.data == 0)
 	return numpy.unravel_index((result * mask).argmax(), board.shape)
